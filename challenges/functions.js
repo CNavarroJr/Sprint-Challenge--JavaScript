397d4e5aa2e39126f543c26ed6931ed15fafa2d9
unknown <navarro89cnp@gmail.com>
2019-12-14 13:58:29 -0600

    these are the challenge files updated

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,11 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume (argument1, argument2, cb) {
+  return cb(argument1, argument2);
+ 
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +19,29 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b) {
+  return a + b;
+}
+
+function multiply (a,b){
+  return a * b;
+}
+
+function greeting (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: It can access the internal variable because it is a closure. As a closure it can access the internal variable even though it is outside of the inner function.
 
 
 const external = "I'm outside the function";
